a92cea253446b5c241947debdfa38c175f1fd9ad
s7rul <eriser-8@student.ltu.se>
2021-06-29 13:36:46 +0200

    Start of POST handler.

---
+++ src/main/java/eu/arrowhead/client/skeleton/provider/LocalConstants.java
@@ -0,0 +1,10 @@
+package eu.arrowhead.client.skeleton.provider;
+
+public class LocalConstants {
+
+    public static final String JAR_DEPLOY_URL = "/deploy_jar";
+
+    private LocalConstants() {
+        throw new UnsupportedOperationException();
+    }
+}
--- src/main/java/eu/arrowhead/client/skeleton/provider/controller/ProviderController.java
+++ src/main/java/eu/arrowhead/client/skeleton/provider/controller/ProviderController.java
@@ -1,9 +1,20 @@
 package eu.arrowhead.client.skeleton.provider.controller;
 
+import eu.arrowhead.client.skeleton.provider.LocalConstants;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import eu.arrowhead.common.CommonConstants;
+
+//import eu.arrowhead.client.skeleton.provider.LocalConstants;
+
+import eu.arrowhead.client.skeleton.provider.LocalConstants;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
 
 @RestController
 public class ProviderController {
@@ -15,6 +26,19 @@ public class ProviderController {
 
 	//=================================================================================================
 	// methods
+
+	@PostMapping(LocalConstants.JAR_DEPLOY_URL)
+	public String handleJarDeploy(@RequestParam("file")MultipartFile file) {
+		File t = new File("/home/s7rul/tmptest.jar");
+		try {
+			file.transferTo(t);
+			return "upploaded!";
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.out.println(e);
+			return "Failed";
+		}
+	}
 
 	//-------------------------------------------------------------------------------------------------
 	@GetMapping(path = CommonConstants.ECHO_URI)
