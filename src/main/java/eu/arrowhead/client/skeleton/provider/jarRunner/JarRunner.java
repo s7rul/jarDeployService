7cac92acf2438d14a890de20d168382eec47a39b
s7rul <eriser-8@student.ltu.se>
2021-06-29 10:45:15 +0200

    class to run and stop jar file

---
+++ src/main/java/eu/arrowhead/client/skeleton/provider/jarRunner/JarRunner.java
@@ -0,0 +1,49 @@
+package eu.arrowhead.client.skeleton.provider.jarRunner;
+
+import java.io.File;
+
+public class JarRunner {
+    File workingDir;
+    File log;
+    String jarName;
+    Process proc;
+
+    public JarRunner(String workingDir, String logPath, String jarName) {
+        this.workingDir = new File(workingDir);
+        this.log = new File(logPath);
+        this.jarName = jarName;
+        this.proc = null;
+    }
+
+    public void run() {
+        ProcessBuilder pb = new ProcessBuilder("java", "-jar", this.jarName);
+        pb.directory(this.workingDir); // set working directory
+        pb.redirectErrorStream(true);
+        pb.redirectOutput(log); // set log file
+        try {
+            this.proc = pb.start();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public void stop() {
+        if (this.proc != null) {
+            this.proc.destroy();
+        }
+    }
+
+    public void forceStop() {
+        if (this.proc != null) {
+            this.proc.destroyForcibly();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Testing to start jar file.");
+        JarRunner t1 = new JarRunner("/home/s7rul/code/InterfaceLightweight/target", "/home/s7rul/tmplog.log", "InterfaceLightweight-1.0.jar");
+        t1.run();
+        Thread.sleep(4000);
+        t1.stop();
+    }
+}
