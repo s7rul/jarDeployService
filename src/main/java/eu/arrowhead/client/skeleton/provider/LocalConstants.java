5660fe64714430bef0f6564ea91a788c0a3c996d
s7rul <eriser-8@student.ltu.se>
2021-07-01 08:55:25 +0200

    small stuff

--- src/main/java/eu/arrowhead/client/skeleton/provider/LocalConstants.java
+++ src/main/java/eu/arrowhead/client/skeleton/provider/LocalConstants.java
@@ -6,8 +6,9 @@ public class LocalConstants {
     public static final String JAR_DEPLOY_SERVICE_DEFINITION = "deploy_jar";
     public static final String JAR_FILES_DIR = "/home/s7rul/jar_files_tmp";
 
-    public static final String INTERFACE_SECURE = "HTTP-SECURE";
-    public static final String INTERFACE_INSECURE = "HTTP-INSECURE";
+    public static final String INTERFACE_SECURE = "https";
+    public static final String INTERFACE_INSECURE = "HTTP-INSECURE-JSON";
+    //public static final String INTERFACE_INSECURE = "http";
     public static final String HTTP_METHOD = "http-method";
 
     private LocalConstants() {
