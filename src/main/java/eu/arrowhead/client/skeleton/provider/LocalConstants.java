85f6a0b62aca6e256aca7f8fcda9833afe755356
s7rul <eriser-8@student.ltu.se>
2021-07-06 09:11:51 +0200

    Making it secure

--- src/main/java/eu/arrowhead/client/skeleton/provider/LocalConstants.java
+++ src/main/java/eu/arrowhead/client/skeleton/provider/LocalConstants.java
@@ -6,7 +6,7 @@ public class LocalConstants {
     public static final String JAR_DEPLOY_SERVICE_DEFINITION = "deploy_jar";
     public static final String JAR_FILES_DIR = "/home/s7rul/jar_files_tmp";
 
-    public static final String INTERFACE_SECURE = "https";
+    public static final String INTERFACE_SECURE = "HTTP-SECURE-JSON";
     public static final String INTERFACE_INSECURE = "HTTP-INSECURE-JSON";
     //public static final String INTERFACE_INSECURE = "http";
     public static final String HTTP_METHOD = "http-method";
